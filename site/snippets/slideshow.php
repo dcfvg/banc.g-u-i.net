565df8a351edc1d872a1aa944d614571fb5af611
dcfvg <qwsx@dcfvg.com>
2014-11-03 10:04:04 +0100

    template

--- site/snippets/slideshow.php
+++ site/snippets/slideshow.php
@@ -4,7 +4,10 @@
 			<?php 
 			$active = "active";
 			foreach($p->images() AS $image): ?>
- 				  	<?php echo thumb($image, array('width' => 1920, 'height' => 1280, 'crop' => true )); ?>
+ 				  	
+ 				  	<a data-toggle="tooltip" data-placement="left"  data-original-title="<?=$image->caption(); ?>" href="#">
+ 				  		<?= thumb($image, array('width' => 1920, 'height' => 1280, 'crop' => true )); ?>
+ 				  	</a>
 			<?php 
 			$active = "";
 			endforeach ?>
